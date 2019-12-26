b29b72fa2a328fdfe8ddb763d7d837a10d14c74e
Naandalist <listiananda.apriliawan@gmail.com>
2019-12-26 10:47:30 +0700

    clean configs

--- src/components/Configs.jsx
+++ src/components/Configs.jsx
@@ -13,17 +13,15 @@ export const getDataWidgets = async () => {
 
 export const getSelectedWidgets = async slug => {
   try {
-    console.log("slug dah nyampe di get selected", slug);
     const result = await Axios.get(`${baseUrl}/${slug}`);
     return result.data;
   } catch (err) {
-    return console.log("ERROR GET DATA WIDGETS: ", err);
+    return console.log("ERROR GET DATA SELECTED WIDGETS: ", err);
   }
 };
 
 export const postDataWidgets = async data => {
   try {
-    console.log("isi datanya adalah: ", data);
     const result = await Axios.post(`${baseUrl}`, {
       name: data.widgetName,
       price: Number(data.price),
@@ -31,14 +29,12 @@ export const postDataWidgets = async data => {
     });
     return result;
   } catch (err) {
-    return console.log("ERROR GET DATA WIDGETS: ", err);
+    return console.log("ERROR POST DATA WIDGETS: ", err);
   }
 };
 
 export const editDataWidgets = async (data, id) => {
   try {
-    console.log("isi datanya di PUT adalah: ", data);
-    console.log("isi id nya di PUT adalah: ", id);
     const result = await Axios.put(`${baseUrl}/${id}`, {
       name: data.name,
       price: Number(data.price),
@@ -46,16 +42,15 @@ export const editDataWidgets = async (data, id) => {
     });
     return result;
   } catch (err) {
-    return console.log("ERROR GET DATA WIDGETS: ", err);
+    return console.log("ERROR EDIT DATA WIDGETS: ", err);
   }
 };
 
 export const deleteDataWidgets = async id => {
   try {
-    console.log("isi datanya adalah: ", id);
     const result = await Axios.delete(`${baseUrl}/${id}`);
     return result;
   } catch (err) {
-    return console.log("ERROR GET DATA WIDGETS: ", err);
+    return console.log("ERROR DELETE DATA WIDGETS: ", err);
   }
 };
